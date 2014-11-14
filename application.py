91e04c52c2b2fcf6de2c9e9b7d2d5d395451dacf
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-11-14 07:18:13 +0100

    Add celery and separate tasks to file

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import re
 from flask import Flask, request
 from fabric.api import local
 from flask import render_template
+from .tasks import _create_container, _create_nginx_proxy
 app = Flask(__name__)
 
 from docker import Client
@@ -26,29 +27,6 @@ def home():
         setup_done = True
 
     return render_template('home.html', name=name, setup_done=setup_done)
-
-
-def _create_container(name):
-    """
-    Create a container with the name
-    """
-    container = c.create_container(
-        image=IMAGE,
-        hostname='%s.docker.openlabs.us' % name,
-        name=name
-    )
-    print container
-    return container
-
-
-def _create_nginx_proxy(name, port):
-    """
-    Create an nginx proxy for the given upstream
-    """
-    nginx_conf = render_template('nginx_conf.jinja', name=name, port=port)
-    with open('/etc/nginx/sites-enabled/%s.conf' % name, 'w') as f:
-        f.write(nginx_conf)
-    local('service nginx reload')
 
 
 if __name__ == "__main__":
---
+++ tasks.py
@@ -0,0 +1,29 @@
+from celery import Celery
+
+app = Celery('tasks', broker='redis://localhost')
+
+
+@app.task
+def _create_container(name):
+    """
+    Create a container with the name
+    """
+    container = c.create_container(
+        image=IMAGE,
+        hostname='%s.docker.openlabs.us' % name,
+        name=name
+    )
+    print container
+    return container
+
+
+@app.task
+def _create_nginx_proxy(name, port):
+    """
+    Create an nginx proxy for the given upstream
+    """
+    nginx_conf = render_template('nginx_conf.jinja', name=name, port=port)
+    with open('/etc/nginx/sites-enabled/%s.conf' % name, 'w') as f:
+        f.write(nginx_conf)
+    local('service nginx reload')
+
